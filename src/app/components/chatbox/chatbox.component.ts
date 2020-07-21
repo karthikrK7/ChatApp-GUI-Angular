927916acc30c64fbc969a2198890bc855285eda8
karthikeyan kesavan <karthikeyan.kesavanr@gmail.com>
2020-07-21 13:59:42 +0530

    loader css

--- src/app/components/chatbox/chatbox.component.ts
+++ src/app/components/chatbox/chatbox.component.ts
@@ -22,15 +22,16 @@ export class ChatboxComponent implements OnInit {
   dp_path: string;
   chatBoxinput: any;
   that: any
-  receiver :any;
-  receiver_Obj : any;
-  user_Id= sessionStorage.getItem('user_Id');
+  receiver: any;
+  receiver_Obj: any;
+  user_Id = sessionStorage.getItem('user_Id');
   payload = {
     text: this.chatBoxinput, sender_id: '', receiver_id: '',
     chat_id: '', msg_Type: '', groupChatId: '', singleChatId: ''
   };
   constructor(private chatAppService: ChatappService, private commonUtils: CommonutilsService, private router: Router) {
     this.initializeWebSocketConnection();
+    this.currentUser_id = sessionStorage.getItem('user_id');
   }
 
   public stompClient;
@@ -57,7 +58,9 @@ export class ChatboxComponent implements OnInit {
   }
 
   getConvo(chat_id) {
-   // this.currentUser_chatid=chat_id.chatId;
+    // this.currentUser_chatid=chat_id.chatId;
+    
+    this.preloader();
     this.chatAppService.getConversation(this.access_token, chat_id).subscribe(
       response => {
         this.conversations = response;
@@ -68,10 +71,16 @@ export class ChatboxComponent implements OnInit {
       }
     );
     $(".card-title").html(chat_id.name);
+    this.receiver = chat_id;
+    this.payload.chat_id = chat_id.chatId;
+    
+    setTimeout(this.scrollerheight, 50);
   }
+
+  
   sendmsg() {
+    console.log(this.payload)
     this.payload.text = this.chatBoxinput;
-    this.payload.chat_id = '6';
     this.stompClient.send('/app/send/message', {}, JSON.stringify(this.payload));
   }
 
@@ -87,9 +96,51 @@ export class ChatboxComponent implements OnInit {
       that.stompClient.subscribe('/message', (message) => {
         if (message.body) {
           console.log(message.body)
+          //{"text":"hey h r u","sender_id":1,"receiver_id":2,"chat_id":1,"msg_Type":0,"groupChatId":"","singleChatId":""}
+
+          var resultJSON = JSON.parse(message.body);
+          var div1, spandiv1, spandiv2;
+          if (resultJSON.SentBy == sessionStorage.getItem('user_id')) {
+            div1 = "direct-chat-msg right";
+            spandiv1 = "float-right";
+            spandiv2 = "float-left";
+          } else {
+            div1 = "direct-chat-msg";
+            spandiv1 = "float-left";
+            spandiv2 = "float-right";
+          }
+          var chatthread = ' <div class="' + div1 + '"><div class="direct-chat-infos clearfix">';
+          chatthread += '<span class="' + spandiv1 + ' direct-chat-name">' + resultJSON.SentByName + '</span>';
+          //var chatthread = ' <div [ngClass]="' + resultJSON.SentBy + ' == ' + sessionStorage.getItem('user_id')  + '? \'direct-chat-msg right\':\'direct-chat-msg\'"><div class="direct-chat-infos clearfix">';
+          //chatthread += '<span [ngClass]="' + resultJSON.SentBy + ' == ' + sessionStorage.getItem('user_id')  + ' ? \'float-right\' : \'float-left\'" class="direct-chat-name">' + resultJSON.SentByName + '</span>';
+          chatthread += '<span';
+          var dt = resultJSON.createdTime;
+          const options = {  year: 'numeric', month: 'short', day: 'numeric'};
+
+          chatthread += ' class="' + spandiv2 + '">'+new Date(dt).toLocaleDateString('en-IN',options)+'</span>';
+          //chatthread += '[ngClass]="{ \'float-left\' : ' + resultJSON.SentBy + ' == ' + sessionStorage.getItem('user_id')  + ' ,\'float-right\' : ' + resultJSON.SentBy + ' != ' + sessionStorage.getItem('user_id')  + ' }">{{' + resultJSON.createdTime + '|date:\'medium\'}}</span>';
+          chatthread += '</div>';
+          chatthread += '<img class="direct-chat-img" src="' + this.dp_path + '" alt="message user image">';
+          chatthread += '<div class="direct-chat-text">';
+          chatthread += resultJSON.Message;
+          chatthread += '</div>';
+          chatthread += '</div>';
+          $(".direct-chat-messages").append(chatthread);
         }
+        $(".direct-chat-messages").scrollTop($(".direct-chat-messages")[0].scrollHeight);
       });
     });
+  }
+
+  scrollerheight(){
+    $(".direct-chat-messages").scrollTop($(".direct-chat-messages")[0].scrollHeight);
+  }
+  preloader(){
+    $("#preloader").attr("class","show");
+   var value = function(){
+      $("#preloader").attr("class","hide")
+    }
+    setTimeout(value, 100);
   }
 
 }
